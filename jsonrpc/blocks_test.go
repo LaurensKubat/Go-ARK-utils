ba33722a4ca3085ba66184099515524634b2c2c3
Laurens <laurens.kubat@gmail.com>
2019-01-12 15:38:22 +0100

    WIP on tests

--- jsonrpc/blocks_test.go
+++ jsonrpc/blocks_test.go
@@ -1,1 +1,25 @@
 package jsonrpc
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestBlocks_Info(t *testing.T) {
+	c, err := newTestClient()
+	require.NoError(t, err)
+	res, err := c.Blocks.Info("123141")
+	assert.NoError(t, err)
+	assert.NotNil(t, res.Result)
+	assert.NotNil(t, res.ID)
+	assert.NotNil(t, res.Result)
+}
+
+func TestBlocks_Latest(t *testing.T) {
+
+}
+
+func TestBlocks_Transactions(t *testing.T) {
+
+}
--- jsonrpc/client_test.go
+++ jsonrpc/client_test.go
@@ -5,9 +5,20 @@ import (
 	"testing"
 )
 
+const testurl = "https://api.arkcoin.net/:8080"
+
+// newTestClient is used to get a client for testing purposes
+func newTestClient() (*Client, error) {
+	c, err := NewClient(testurl)
+	if err != nil {
+		return nil, err
+	}
+	return c, nil
+}
+
 func Test_NewClient(t *testing.T) {
 	assert.NotPanics(t, func() {
-		_, err := newClient("0.0.0.0:8080")
+		_, err := NewClient(testurl)
 		if err != nil {
 			panic(err)
 		}
